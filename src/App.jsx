6aabb03c3c24411a688389a7c738bf736a695566
jaleel <shaikabduljaleel1214@gmail.com>
2025-05-26 15:38:12 +0530

    updated logo

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ const loadTrendingMovies = async () => {
       <div className="pattern">
         <div className="wrapper">
           <header>
-            <img src="./logo.png" alt="logo" className='mb-[-215px] mt-[-180px]' />
+            <img src="./logo.png" alt="logo" /> 
             <img src="./hero.png" alt="Hero Banner" />
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
           </header>
